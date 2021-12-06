0919fa7a302466980ee8153606e1d779f83c16ba
tanvir2911 <hossainmozammel27@gmail.com>
2021-12-06 20:22:36 +0600

    post method to save new user to the database and put method and options upsert to check a user in the database if not create one

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function run() {
         await client.connect();
         const database = client.db('doctors_portal');
         const appointmentsCollection = database.collection('appointments');
+        const usersCollection = database.collection('users');
 
         app.get('/appointments', async (req, res) => {
             const email = req.query.email;
@@ -32,6 +33,25 @@ async function run() {
             const appointment = req.body;
             const result = await appointmentsCollection.insertOne(appointment);
             console.log(result);
+            res.json(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const users = req.body;
+            const result = await usersCollection.insertOne(users);
+            console.log(result);
+            res.json(result);
+        })
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            console.log('put', user)
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.json(result);
         })
 
