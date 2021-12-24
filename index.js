41042a9347fa462fd09a40eecec4ff5736c871ed
tanvir2911 <hossainmozammel27@gmail.com>
2021-12-24 08:12:33 +0600

    doctors api added

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const admin = require('firebase-admin');
-const port = process.env.PORT || 5000;
 const app = express();
+const fileUpload = require('express-fileupload');
+
+const port = process.env.PORT || 5000;
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use(fileUpload());
 
 
 
@@ -48,6 +51,8 @@ async function run() {
         const database = client.db('doctors_portal');
         const appointmentsCollection = database.collection('appointments');
         const usersCollection = database.collection('users');
+        const doctorsCollection = database.collection('doctors');
+
 
         app.get('/appointments', verifyToken, async (req, res) => {
             const email = req.query.email;
@@ -82,6 +87,30 @@ async function run() {
             }
             const result = await appointmentsCollection.updateOne(filter, updateDoc);
             res.json(result);
+        })
+
+        app.get('/doctors', async (req, res) => {
+            const cursor = doctorsCollection.find({});
+            const doctors = await cursor.toArray();
+            res.json(doctors);
+        })
+
+        app.post('/doctors', async (req, res) => {
+            // console.log('body',req.body);
+            // console.log('files',req.files);
+            const name = req.body.name;
+            const email = req.body.email;
+            const pic = req.files.image;
+            const picData = pic.data;
+            const encodedPic = picData.toString('base64');
+            const imageBuffer = Buffer.from(encodedPic, 'base64');
+            const doctor = {
+                name,
+                email,
+                image: imageBuffer
+            }
+            const result = await doctorsCollection.insertOne(doctor);
+            res.json(result)
         })
 
         app.get('/users/:email', async (req, res) => {
